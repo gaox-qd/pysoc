380cf9db1fc46e30c18ac8149a932c803d93af09
oliver-s-lee <osl@st-andrews.ac.uk>
2021-03-21 14:48:10 +0000

    Expanded Zeff values for transition metals from J. Phys. Chem. A 1998, 102, 50, 10430–10435

--- molsoc/molsoc0.1/source/sozeff.f
+++ molsoc/molsoc0.1/source/sozeff.f
@@ -43,6 +43,32 @@ C
         SOZEFF = 1234
       ELSE IF (ATOM == 30) THEN
         SOZEFF = 330
+C	 Zeff taken from J. Phys. Chem. A 1998, 102, 50, 10430–10435:
+C    First row transition metals.
+      ELSE IF (ATOM >= 21 .AND. ATOM <= 30) THEN
+        SOZEFF = ATOM * (0.385 + 0.025 * (NEVAL(ATOM) -2))
+C    Second row transition metals.
+      ELSE IF (ATOM >= 39 .AND. ATOM <= 48) THEN
+        SOZEFF = ATOM * (4.680 + 0.060 * (NEVAL(ATOM) -2))
+C    Third row transition metals.
+      ELSE IF (ATOM == 72) THEN
+        SOZEFF = 1025.28
+      ELSE IF (ATOM == 73) THEN
+        SOZEFF = 1049.74
+      ELSE IF (ATOM == 74) THEN
+        SOZEFF = 1074.48
+      ELSE IF (ATOM == 75) THEN
+        SOZEFF = 1099.5
+      ELSE IF (ATOM == 76) THEN
+        SOZEFF = 1124.8
+      ELSE IF (ATOM == 77) THEN
+        SOZEFF = 1150.38
+      ELSE IF (ATOM == 78) THEN
+        SOZEFF = 1176.24
+      ELSE IF (ATOM == 79) THEN
+        SOZEFF = 1202.38
+      ELSE IF (ATOM == 80) THEN
+        SOZEFF = 1228.8
       ELSE
         WRITE(OUT,1000) ATOM
         STOP
